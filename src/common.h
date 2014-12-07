9c7f79be68c02716c753c87a6db158e18cd326d4
Jack Atkinson <jack.atkinson@outlook.com>
2014-12-06 22:46:40 -0600

    Initial Commit

---
+++ src/common.h
@@ -0,0 +1,33 @@
+/*
+	common.h
+
+	Contributed by Jack Atkinson <jack.atkinson@outlook.com>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program, see the file COPYING. If not, see
+	<http://www.gnu.org/licenses/>.
+*/
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+typedef uint8_t  BYTE;
+typedef uint16_t WORD;
+typedef uint32_t DWORD;
+
+#endif
